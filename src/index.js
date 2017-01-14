7647e39e7f3bb7ea20e27e996457f30ea85a29f7
Juan Carlos <juancafe2@gmail.com>
2017-01-14 00:50:30 -0400

    Components

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
- 
+import React from 'react'
+import ReactDOM from 'react-dom'
+// Create a new component
+// This component will produce some HTML
+
+const App = () => <div>Hi</div>;
+
+// Take this component;s generated HTML and put
+// in the DOM
+
+ReactDOM.render(<App />, document.querySelector('.container'))
