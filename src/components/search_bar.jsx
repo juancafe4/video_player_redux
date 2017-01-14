3cb9c06d94ff8e4e3b8122525430a25853b8c9c5
Juan Carlos <juancafe2@gmail.com>
2017-01-14 18:29:48 -0400

    Able to search for videos

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -4,14 +4,18 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
     
-    this.state = { term: 'Starting' };
+    this.state = { term: '' };
+  }
+  onInputChange(term) {
+    this.setState({ term });
+    this.props.onSearchTermChange(term);
   }
   render() {
     return (
-      <div>
+      <div className="search-bar">
         <input 
           value = { this.state.term }
-          onChange={ event => this.setState({ term: event.target.value} ) } type="text"
+          onChange={ event => this.onInputChange(event.target.value) } type="text"
         />
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -19,9 +19,12 @@ class App extends Component {
       videos: [],
       selectedVideo : null,
     };
+    
+    this.videoSearch('surfboards')    
+  }
 
-
-    YTSearch({ key: API_KEY, term: 'surfboards'}, videos => {
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term}, videos => {
       this.setState({ 
         videos,
         selectedVideo: videos[0]
@@ -31,7 +34,7 @@ class App extends Component {
   render() {
     return (
       <div> 
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
         <VideoDetail video={this.state.selectedVideo} />
         <VideoList 
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
